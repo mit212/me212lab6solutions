15c2f990f568856a08556db44331544ba539ede9
Jacob Guggenheim <jguggenh@mit.edu>
2019-03-11 12:40:36 -0400

    adsf

---
+++ catkin_ws/src/me212cv/scripts/canny.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+# 2.12 Lab 4 object detection: a node for de-noising
+# Luke Roberto Oct 2017
+
+import rospy
+import numpy as np
+import cv2  # OpenCV module
+from matplotlib import pyplot as plt
+import time
+
+from sensor_msgs.msg import Image, CameraInfo
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point, Pose, Twist, Vector3, Quaternion
+from std_msgs.msg import ColorRGBA
+
+from cv_bridge import CvBridge, CvBridgeError
+import message_filters
+import math
+
+rospy.init_node('canny', anonymous=True)
+
+# Bridge to convert ROS Image type to OpenCV Image type
+cv_bridge = CvBridge()
+
+def main():
+    rospy.Subscriber('/usb_cam/image_raw', Image, rosHTransformCallback)
+    print("Subscribing")
+    rospy.spin()
+
+
+def rosHTransformCallback(msg):
+    # 1. convert ROS image to opencv format
+    try:
+        cv_image = cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+
+    # 2. visualize it in a cv window
+    cv2.imshow("Original_Image", cv_image)
+    cv2.waitKey(3)
+
+    grayIm = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+    cannyIm = cv2.Canny(grayIm,10,500,apertureSize = 3)   # Canny edge detector to make it easier for hough transform to "agree" on lines
+    cv2.imshow("Canny_Image", cannyIm)
+    cv2.waitKey(3)
+
+if __name__=='__main__':
+    main()
---
+++ catkin_ws/src/me212cv/scripts/hough.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+# 2.12 Lab 4 object detection: a node for de-noising
+# Luke Roberto Oct 2017
+
+import rospy
+import numpy as np
+import cv2  # OpenCV module
+from matplotlib import pyplot as plt
+import time
+
+from sensor_msgs.msg import Image, CameraInfo
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point, Pose, Twist, Vector3, Quaternion
+from std_msgs.msg import ColorRGBA
+
+from cv_bridge import CvBridge, CvBridgeError
+import message_filters
+import math
+
+rospy.init_node('hough', anonymous=True)
+
+# Bridge to convert ROS Image type to OpenCV Image type
+cv_bridge = CvBridge()
+
+def main():
+    rospy.Subscriber('/usb_cam/image_raw', Image, rosHTransformCallback)
+    print("Subscribing")
+    rospy.spin()
+
+
+
+def rosHTransformCallback(msg):
+    # convert ROS image to opencv format
+    try:
+        cv_image = cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+
+    # visualize it in a cv window
+    cv2.imshow("Original_Image", cv_image)
+    cv2.waitKey(3)
+
+    # gray and blur
+    resizeIm = cv2.resize(cv_image, (0,0), fx=0.25, fy=0.25)
+    blurIm = cv2.medianBlur(resizeIm,5)
+    grayIm = cv2.cvtColor(blurIm,cv2.COLOR_BGR2GRAY) # Convert to grascale image
+
+    # hough
+    circles = cv2.HoughCircles(grayIm,cv2.HOUGH_GRADIENT,1.2,20,param1=100,param2=50, minRadius=0,maxRadius=60)
+    print(circles)
+    if circles is not None:
+        circles = (np.around(circles))
+        for i in circles[0,:]:
+            # draw circle
+            cv2.circle(resizeIm,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(resizeIm,(i[0],i[1]),2,(0,0,255),3)
+
+
+    cv2.imshow("Hough Circle Detection", resizeIm)
+    cv2.waitKey(3)
+
+if __name__=='__main__':
+    main()
